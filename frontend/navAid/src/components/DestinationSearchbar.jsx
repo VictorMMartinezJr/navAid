97dad90eeadfcace35d88d18d25809fb7d9d65f6
Victor <victorm.martinezjr@outlook.com>
2025-07-06 01:50:01 -0400

    add context api to Destination Searchbar

--- frontend/navAid/src/components/DestinationSearchbar.jsx
+++ frontend/navAid/src/components/DestinationSearchbar.jsx
@@ -1,12 +1,31 @@
+import { useContext } from "react";
 import { assets } from "../assets/assets";
+import NavigationContext from "../context/NavigationContext";
 
 const DestinationSearchbar = () => {
+  const { destination, setDestination } = useContext(NavigationContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (destination) {
+    }
+  };
+
   return (
-    <form className="relative flex justify-between items-center w-[70%]">
+    <form
+      onSubmit={handleSubmit}
+      className="relative flex justify-between items-center w-[70%]"
+    >
       <input
         className="border-2  border-gray-400 rounded-full p-2 w-full"
         type="text"
         placeholder="Destination"
+        value={destination}
+        onChange={(e) => {
+          const value = e.target.value.trim();
+          setDestination(value === "" ? null : value);
+        }}
       />
       <img
         src={assets.searchIcon}
