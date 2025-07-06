93ab692e84929e486026f7d1ae54ac55b4aff48b
Victor <victorm.martinezjr@outlook.com>
2025-07-06 01:14:56 -0400

    create Destination and StartingPoint Searchbars

---
+++ frontend/navAid/src/assets/assets.js
@@ -0,0 +1,7 @@
+import logo from "./navaid.svg";
+import searchIcon from "./icon_search.png";
+
+export const assets = {
+  logo,
+  searchIcon,
+};
---
+++ frontend/navAid/src/components/StartingPointSearchbar.jsx
@@ -0,0 +1,20 @@
+import { assets } from "../assets/assets";
+
+const StartingPointSearchbar = () => {
+  return (
+    <form className="relative flex justify-between items-center flex-1/2 ">
+      <input
+        className="border-2  border-gray-400 rounded-full p-2 w-full"
+        type="text"
+        placeholder="Starting Point"
+      />
+      <img
+        src={assets.searchIcon}
+        alt="Search icon"
+        className="absolute right-4 h-8 w-8"
+      />
+    </form>
+  );
+};
+
+export default StartingPointSearchbar;
