459bde4b564f4295d3cf58cac4e0604d03a91a2a
Victor <victorm.martinezjr@outlook.com>
2025-07-06 01:37:18 -0400

    create context api

---
+++ frontend/navAid/src/components/context/NavigationContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+
+const NavigationContext = createContext();
+
+export const NavigationProvider = ({ children }) => {
+  const [startingPoint, setStartingPoint] = useState(null);
+  const [destination, setDestination] = useState(null);
+  const [path, setPath] = useState([]);
+
+  const contextValues = {
+    startingPoint,
+    setStartingPoint,
+    destination,
+    setDestination,
+    path,
+    setPath,
+  };
+
+  return (
+    <NavigationContext.Provider value={contextValues}>
+      {children}
+    </NavigationContext.Provider>
+  );
+};
+
+export default NavigationContext;
