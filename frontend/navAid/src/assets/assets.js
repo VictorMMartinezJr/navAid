220f89373a977d6077b24fc2be3795ad40dcdaba
Victor <victorm.martinezjr@outlook.com>
2025-07-12 23:14:42 -0400

    replace map with map without colored hallways

--- frontend/navAid/src/assets/assets.js
+++ frontend/navAid/src/assets/assets.js
@@ -1,17 +1,17 @@
-import buildingAMap from "./buildingAMap.jpg";
 import logo from "./navaid.svg";
 import searchIcon from "./icon_search.png";
 import onestopIcon from "./icon_onestop.png";
 import bookstoreIcon from "./icon_bookstore.png";
 import libraryIcon from "./icon_library.png";
 import policeIcon from "./icon_police.png";
+import map from "./buildingMap.jpg";
 
 export const assets = {
-  buildingAMap,
   logo,
   searchIcon,
   onestopIcon,
   bookstoreIcon,
   libraryIcon,
   policeIcon,
+  map,
 };
