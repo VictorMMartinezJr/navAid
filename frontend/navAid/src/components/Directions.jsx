2361bb5bfb36cc2451fa237d4e3073ca720a507c
Victor <victorm.martinezjr@outlook.com>
2025-07-18 16:31:02 -0400

    add animation to each direction

--- frontend/navAid/src/components/Directions.jsx
+++ frontend/navAid/src/components/Directions.jsx
@@ -3,6 +3,8 @@ import NavigationContext from "../context/NavigationContext";
 import { FcRight, FcLeft, FcUp } from "react-icons/fc";
 import { MdLocationPin } from "react-icons/md";
 import { rooms } from "../util/rooms";
+import { graph } from "../util/graph";
+import { easeInOut, motion } from "framer-motion";
 
 const Directions = () => {
   const {
@@ -15,13 +17,20 @@ const Directions = () => {
     path,
     currentStep,
     setCurrentStep,
+    initialStageResetFn,
   } = useContext(NavigationContext);
   const [arrived, setArrived] = useState(false);
   const [directionArrow, setDirectionArrow] = useState("");
+  const [isCooldown, setIsCooldown] = useState(false);
 
-  const steps = generateInstructions(path);
+  const steps = generateInstructions(path, graph);
 
   const handleClick = () => {
+    if (isCooldown) return;
+
+    setIsCooldown(true);
+    setTimeout(() => setIsCooldown(false), 500);
+
     // Reset app
     if (arrived) {
       setStartingPoint("Main Entrance");
@@ -30,6 +39,10 @@ const Directions = () => {
       setLinePath([]);
       setStartAndDestinationSubmitted(false);
       setCurrentStep(0);
+      setArrived(false);
+      if (initialStageResetFn) {
+        initialStageResetFn();
+      }
 
       return;
     }
@@ -60,6 +73,7 @@ const Directions = () => {
     return <FcUp />;
   };
 
+  // Set which arrow to show in direction
   useEffect(() => {
     if (steps[currentStep].toLowerCase().includes("right")) {
       setDirectionArrow("right");
@@ -72,15 +86,22 @@ const Directions = () => {
 
   return (
     <div className="absolute top-0 left-0 z-20 h-[10vh] w-full bg-[#303030] flex justify-between items-center px-2">
-      <div className="flex justify-center items-center gap-4 text-white text-4xl">
+      <motion.div
+        className="flex justify-center items-center gap-4 text-white text-4xl"
+        key={currentStep}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, ease: easeInOut }}
+      >
         {getDirectionIcon()}
         <p className="text-white text-xl font-bold">
           {arrived ? "Arrived" : steps[currentStep]}
         </p>
-      </div>
+      </motion.div>
       <button
         className="px-4 py-2 rounded-lg font-bold text-white bg-blue-600 cursor-pointer"
         onClick={handleClick}
+        disabled={isCooldown}
       >
         {arrived ? "Restart" : "Next"}
       </button>
